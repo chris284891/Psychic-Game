283d9903a4864533baa45c512828a1b7e8a6e40d
chris284891 <chris284891@gmail.com>
2019-06-06 18:34:34 -0400

    completed game

--- assets/game.js
+++ assets/game.js
@@ -2,31 +2,44 @@ var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s
 var wins = 0;
 var losses = 0;
 var guessesleft = 9;
+var storeduserguess = []
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesleftText = document.getElementById("guessesleft-text");
 var userguessesText = document.getElementById("usersguesses-text");
+console.log(winsText)
+
+// trying to make it so if user guesses a letter not in array computerGuess they get a loss. if it is they get a win. also 
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
+    storeduserguess.push(userGuess)
+    console.log(userGuess)
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    if (userGuess === computerChoices) {
-        if (userGuess === computerGuess) {
-            wins++;
-        }
-        else if (userGuess !== computerGuess) {
-            guessesleft--;
-        }
-        else (userGuess !== computerGuess) {
-            losses++;
-        }
-
-        winsText.textContent = "wins: " + wins;
-        lossesText.textContent = "losses: " + losses;
-        guessesleftText.textContent = "guesses left: " + guessesleft;
-        userguessesText.textContent = "you guesses: " + ???;
+    console.log(wins)
+    console.log(losses)
+    console.log(guessesleft)
+    if (userGuess === computerGuess) {
+        wins++;
+        storeduserguess = [];
+        guessesleft = 9;
     }
-};
+    if (userGuess !== computerGuess) {
+        guessesleft--;
+    }
+    if (guessesleft === 0) {
+        losses++;0
+        guessesleft = 9;
+        storeduserguess = [];
+        
+    }
+    // i don't know how to make game pick new letter after loss
+    // i don't know how to make read out what user guessed
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesleftText.textContent = "Guesses left: " + guessesleft;
+    userguessesText.textContent = "You guessed: " + storeduserguess;
+    console.log(computerGuess)
 
-console.log(computerGuess)
+};

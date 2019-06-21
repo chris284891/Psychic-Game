634bf1c2e0fa8428ea995e1c81601720809739eb
chris284891 <chris284891@gmail.com>
2019-06-21 13:40:02 -0400

    Made it so only alphabet keys work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,28 +11,34 @@ var userguessesText = document.getElementById("usersguesses-text");
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 // I couldn't figure out how to make the onkeyup function only accept alphabet keys (still trying(stackoverflow))
 document.onkeyup = function (event) {
-    var userGuess = event.key;
-    storeduserguess.push(userGuess);
-    if (userGuess === computerGuess) {
-        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        wins++;
-        guessesleft = 9;
-        storeduserguess = [];
-    }
-    else if (userGuess !== computerGuess) {
-        guessesleft--;
-    }
-    if (guessesleft === 0) {
-        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        losses++;
-        guessesleft = 9;
-        storeduserguess = [];
-    }
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessesleftText.textContent = "Guesses left: " + guessesleft;
-    userguessesText.textContent = "You guessed: " + storeduserguess;
-    // open up the console if you want to be a cheater...
-    console.log(computerGuess);
-    console.log(userGuess)
+    // if (computerChoices.indexOf(event) >= 0) {
+        if(computerChoices.indexOf(event.key) === -1) {
+            return event.preventDefault();
+          }
+        console.log(event)
+        var userGuess = event.key;
+        storeduserguess.push(userGuess);
+        if (userGuess === computerGuess) {
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            wins++;
+            guessesleft = 9;
+            storeduserguess = [];
+        }
+        else if (userGuess !== computerGuess) {
+            guessesleft--;
+        }
+        if (guessesleft === 0) {
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            losses++;
+            guessesleft = 9;
+            storeduserguess = [];
+        }
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesleftText.textContent = "Guesses left: " + guessesleft;
+        userguessesText.textContent = "You guessed: " + storeduserguess;
+        // open up the console if you want to be a cheater...
+        console.log(computerGuess);
+        console.log(userGuess)
+    // };
 };
